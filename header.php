d1f89d613c66945166c8f73a69e09bd74b79aec6
Przemek Bąchorek <przemek@reddog.systems>
2018-08-29 16:02:17 +0200

    0.6.7

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/styles/master.css">
   <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
   <?php wp_head(); ?>
 </head>
