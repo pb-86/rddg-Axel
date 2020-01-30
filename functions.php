0d4eed4b4ebf54a8987c256f99f29f9d663d5486
Przemek Bąchorek <przemek@reddog.systems>
2020-01-30 18:43:50 +0100

    0.10.4

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
  *
  * @package WordPress
  * @subpackage Axel
- * @version 0.10.3
+ * @version 0.10.4
  */
 
-define( 'AXEL_THEME_VERSION', '0.10.3' );
+define( 'AXEL_THEME_VERSION', '0.10.4' );
 
 define( 'AXEL_THEME_URI', get_template_directory_uri() );
 define( 'AXEL_THEME_IMAGES', AXEL_THEME_URI . '/assets/images' );
--- searchform.php
+++ searchform.php
@@ -4,12 +4,17 @@
  *
  * @package WordPress
  * @subpackage Axel
- * @version 0.8.1
+ * @version 0.10.4
  */
 
 ?>
 
-<form role="search" method="get" action="<?php echo esc_url( home_url() ); ?>">
-	<input type="search" name="s" />
-	<button type="submit"><?php esc_html_e( 'Search', 'axel' ); ?></button>
+<form class="axel-search" role="search" method="get" action="<?php echo esc_url( home_url() ); ?>">
+	<label class="axel-search__label">
+		<?php esc_html_e( 'Search', 'axel' ); ?>
+		<input class="axel-search__input" type="search" name="s">
+	</label>
+	<button class="axel-search__submit" type="submit">
+		<?php esc_html_e( 'Go', 'axel' ); ?>
+	</button>
 </form>
