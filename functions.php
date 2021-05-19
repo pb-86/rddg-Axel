dceecd390f41aad8172220b77fc8459b831e3744
Przemek Bąchorek <przemek@reddog.systems>
2021-05-19 15:10:52 +0200

    1.40
    Fixed: CSS styles of navbars

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
  *
  * @package WordPress
  * @subpackage Axel
- * @since 1.39
+ * @since 1.40
  */
 
-define( 'AXEL_THEME_VERSION', '1.38' );
+define( 'AXEL_THEME_VERSION', '1.40' );
 
 define( 'AXEL_THEME_URI', get_template_directory_uri() );
 define( 'AXEL_THEME_IMAGES', AXEL_THEME_URI . '/assets/images/compressed/' );
