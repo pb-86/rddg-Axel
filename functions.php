221ed96bebe19c910741d72c21bc69e647df07b8
Przemek Bąchorek <przemek@reddog.systems>
2021-06-14 11:32:44 +0200

    1.49
    Fixed: the way that theme displays sidebar

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@
  *
  * @package WordPress
  * @subpackage Axel
- * @since 1.48
+ * @since 1.49
  */
 
-define( 'AXEL_THEME_VERSION', '1.48' );
+define( 'AXEL_THEME_VERSION', '1.49' );
 
 define( 'AXEL_THEME_URI', get_template_directory_uri() );
 define( 'AXEL_THEME_IMAGES', AXEL_THEME_URI . '/assets/images/compressed/' );
--- sidebar.php
+++ sidebar.php
@@ -4,15 +4,13 @@
  *
  * @package WordPress
  * @subpackage Axel
- * @since 1.21
+ * @since 1.49
  */
 
 ?>
 
 <aside class="axel-sidebar">
 	<?php if ( is_active_sidebar( 'axel-sidebar-1' ) ) : ?>
-		<ul id="sidebar">
-			<?php dynamic_sidebar( 'axel-sidebar-1' ); ?>
-		</ul>
+		<?php dynamic_sidebar( 'axel-sidebar-1' ); ?>
 	<?php endif; ?>
 </aside>
