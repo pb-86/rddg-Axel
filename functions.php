36c444e1caa89fcd120a27e776f2f4d63b5227a7
Przemek Bąchorek <przemek@reddog.systems>
2022-04-14 12:46:58 +0200

    Changed functions.php for better code readability

--- functions.php
+++ functions.php
@@ -4,20 +4,32 @@
  *
  * @package WordPress
  * @subpackage Axel
- * @since 1.59
+ * @since 1.60
  */
 
-define( 'AXEL_THEME_VERSION', '1.59' );
+define( 'AXEL_THEME_VERSION', '1.60' );
 
+// Defining useful constants for further use.
 define( 'AXEL_THEME_URI', get_template_directory_uri() );
 define( 'AXEL_THEME_IMAGES', AXEL_THEME_URI . '/assets/images/compressed/' );
 define( 'AXEL_THEME_STYLES', AXEL_THEME_URI . '/assets/styles/css/' );
 define( 'AXEL_THEME_SCRIPTS', AXEL_THEME_URI . '/assets/scripts/minified/' );
 define( 'AXEL_THEME_VENDOR', AXEL_THEME_URI . '/assets/vendor/' );
 
-require_once 'settings/setup.php';        // Basic theme setup.
-require_once 'settings/styles.php';       // Registering CSS styles.
-require_once 'settings/scripts.php';      // Registering JS scripts.
-require_once 'settings/menus.php';        // Registering menus.
-require_once 'settings/sidebars.php';     // Registering sidebar.
-require_once 'settings/optimisation.php'; // Code that helps speed up site.
+// Basic theme setup.
+require_once 'settings/setup.php';
+
+// Registering CSS styles.
+require_once 'settings/styles.php';
+
+// Registering JS scripts.
+require_once 'settings/scripts.php';
+
+// Registering menus.
+require_once 'settings/menus.php';
+
+// Registering sidebars.
+require_once 'settings/sidebars.php';
+
+// Code that helps speed up site.
+require_once 'settings/optimisation.php';
