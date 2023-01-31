0be2a9e23f54aa2ed45e3ec263321f59ec01a653
Przemek Bąchorek <przemek@reddog.systems>
2023-01-31 13:37:04 +0100

    Poprawiono dostępność odnośnika do strony autora

--- template-parts/singular/parts/author.php
+++ template-parts/singular/parts/author.php
@@ -12,9 +12,9 @@
 
 	<?php
 	printf(
-		'<a href="%s" aria-label="%s">%s</a>',
+		'<a href="%s"><span class="screen-reader-text">%s</span> %s</a>',
 		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_html__( 'Zobacz inne wpisy autora ', 'axel' ) . esc_html( get_the_author_meta( 'display_name' ) ),
+		esc_html__( 'Zobacz inne wpisy autora', 'axel' ),
 		esc_html( get_the_author_meta( 'display_name' ) )
 	)
 	?>
