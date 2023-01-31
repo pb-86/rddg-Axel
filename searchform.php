75ffd8a64b1656acaf055bcbfd1526c61c83d7cb
Przemek Bąchorek <przemek@reddog.systems>
2023-01-31 19:03:35 +0100

    Poprawiono prefiks klas CSS dla formularza wyszukiwania

--- searchform.php
+++ searchform.php
@@ -8,8 +8,8 @@
 
 ?>
 
-<form class="axel-search-form" method="get" action="<?php echo esc_url( home_url() ); ?>">
-	<label class="axel-search-form__label" for="search">
+<form class="search-form" method="get" action="<?php echo esc_url( home_url() ); ?>">
+	<label class="search-form__label" for="search">
 
 		<!-- Etykieta -->
 		<span class="screen-reader-text">
@@ -17,10 +17,10 @@
 		</span>
 
 		<!-- Pole wyszukiwania -->
-		<input class="axel-search-form__input" type="search" id="search" name="s" autocomplete="on">
+		<input class="search-form__input" type="search" id="search" name="s" autocomplete="on">
 
 		<!-- Przycisk wyszukiwania -->
-		<button class="axel-button axel-search-form__button" type="submit">
+		<button class="axel-button search-form__button" type="submit">
 			<?php esc_html_e( 'Szukaj', 'axel' ); ?>
 		</button>
 
