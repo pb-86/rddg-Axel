dcef3f580246f14a1df53151fdc23040d382b516
Przemek Bąchorek <przemek@reddog.systems>
2023-01-31 13:45:45 +0100

    Poprawiono poziomy nagłówków

--- templates/template-about.php
+++ templates/template-about.php
@@ -14,9 +14,9 @@ get_header();
 <main class="axel-main" id="axel-main" tabindex="-1">
 
 	<!-- Title -->
-	<h2 class="axel-main__title">
+	<h1 class="axel-main__title">
 		<?php esc_html_e( 'O nas', 'axel' ); ?>
-	</h2>
+	</h1>
 
 </main>
 
