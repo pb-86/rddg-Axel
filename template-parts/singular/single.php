ebb816fffa505c6b4793e9e402097868d5d81b39
Przemek Bąchorek <przemek@reddog.systems>
2023-03-17 13:44:52 +0100

    Poprawiono wygląd sekcji *author*, *date*, *thumbnail* oraz *title*

--- template-parts/singular/single.php
+++ template-parts/singular/single.php
@@ -11,9 +11,9 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
-	get_template_part( 'template-parts/singular/parts/author' );
 	get_template_part( 'template-parts/singular/parts/date' );
 	get_template_part( 'template-parts/singular/parts/title' );
+	get_template_part( 'template-parts/singular/parts/author' );
 	get_template_part( 'template-parts/singular/parts/thumbnail' );
 	get_template_part( 'template-parts/singular/parts/content' );
 	get_template_part( 'template-parts/singular/parts/back-to-home' );
