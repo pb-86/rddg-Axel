26df46915755f740ae138b0444ea444fdaed5380
Przemek Bąchorek <przemek@reddog.systems>
2023-03-17 13:05:57 +0100

    Dodano style do modułu *date*

--- template-parts/singular/parts/date.php
+++ template-parts/singular/parts/date.php
@@ -10,5 +10,5 @@ printf(
 	'<time class="date" datetime="%s">%s %s</time>',
 	esc_attr( get_the_time( 'Y-m-d H:i' ) ),
 	esc_html( human_time_diff( get_post_timestamp(), time() ) ),
-	esc_html__( ' temu!', 'axel' )
+	esc_html__( ' temu', 'axel' )
 );
