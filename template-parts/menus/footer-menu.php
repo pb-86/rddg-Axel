f03efd5f6a3978df59568b994398a9044146ab40
Przemek Bąchorek <przemek@reddog.systems>
2023-03-17 14:38:30 +0100

    Poprawiono obsługę menu

--- template-parts/menus/footer-menu.php
+++ template-parts/menus/footer-menu.php
@@ -6,14 +6,19 @@
  * @subpackage Axel
  */
 
-wp_nav_menu(
-	array(
-		'container'       => 'nav',
-		'container_id'    => 'navbar-footer',
-		'container_class' => 'navbar navbar--footer',
-		'menu'            => 'footer_menu',
-		'menu_id'         => 'menu-footer',
-		'menu_class'      => 'menu menu--footer',
-		'theme_location'  => 'footer_menu',
-	)
-);
+if ( has_nav_menu( 'footer_menu' ) ) {
+
+	wp_nav_menu(
+		array(
+			'container'            => 'nav',
+			'container_id'         => 'navbar-footer',
+			'container_class'      => 'navbar navbar--footer',
+			'container_aria_label' => esc_html__( 'Dolne menu', 'axel' ),
+			'menu'                 => 'footer_menu',
+			'menu_id'              => 'menu-footer',
+			'menu_class'           => 'menu menu--footer',
+			'theme_location'       => 'footer_menu',
+		)
+	);
+
+}
--- template-parts/menus/header-menu.php
+++ template-parts/menus/header-menu.php
@@ -6,14 +6,19 @@
  * @subpackage Axel
  */
 
-wp_nav_menu(
-	array(
-		'container'       => 'nav',
-		'container_id'    => 'navbar-header',
-		'container_class' => 'navbar navbar--header',
-		'menu'            => 'header_menu',
-		'menu_id'         => 'menu-header',
-		'menu_class'      => 'menu menu--header',
-		'theme_location'  => 'header_menu',
-	)
-);
+if ( has_nav_menu( 'header_menu' ) ) {
+
+	wp_nav_menu(
+		array(
+			'container'            => 'nav',
+			'container_id'         => 'navbar-header',
+			'container_class'      => 'navbar navbar--header',
+			'container_aria_label' => esc_html__( 'Górne menu', 'axel' ),
+			'menu'                 => 'header_menu',
+			'menu_id'              => 'menu-header',
+			'menu_class'           => 'menu menu--header',
+			'theme_location'       => 'header_menu',
+		)
+	);
+
+}
