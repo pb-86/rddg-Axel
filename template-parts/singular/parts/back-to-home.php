5b1eb7a6565317820f79d5fc08c0d54e6040ef33
Przemek Bąchorek <przemek@reddog.systems>
2023-03-17 13:52:32 +0100

    Poprawiono wygląd sekcji *back-to-home*

--- template-parts/singular/parts/back-to-home.php
+++ template-parts/singular/parts/back-to-home.php
@@ -7,7 +7,7 @@
  */
 
 printf(
-	'<a class="back-to-home" href="%s">%s</a>',
+	'<div class="back-to-home"><a class="back-to-home__link" href="%s">%s</a>',
 	esc_url( home_url() ),
 	esc_html__( 'Wróć do strony głównej', 'axel' )
 );
