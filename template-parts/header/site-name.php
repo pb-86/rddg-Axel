2be4f29406dd7b0187751ade6345ecd0dfdd70cc
Przemek Bąchorek <przemek@reddog.systems>
2023-03-21 11:43:09 +0100

    Poprawiono wyświetlanie tytułu i opisu strony

--- template-parts/header/site-name.php
+++ template-parts/header/site-name.php
@@ -8,16 +8,24 @@
 
 ?>
 
-<div class="site-name">
+<?php if ( get_bloginfo( 'name' ) || get_bloginfo( 'description' ) ) : ?>
 
-	<div class="site-name__title">
-		<a class="site-name__link" href="<?php echo esc_url( home_url() ); ?>">
-			<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-		</a>
+	<div class="site-name">
+
+		<?php if ( get_bloginfo( 'name' ) ) : ?>
+			<div class="site-name__title">
+				<a class="site-name__link" href="<?php echo esc_url( home_url() ); ?>">
+					<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+				</a>
+			</div>
+		<?php endif; ?>
+
+		<?php if ( get_bloginfo( 'description' ) ) : ?>
+			<div class="site-name__description">
+				<?php echo esc_html( get_bloginfo( 'description' ) ); ?>
+			</div>
+		<?php endif; ?>
+
 	</div>
 
-	<div class="site-name__description">
-		<?php echo esc_html( get_bloginfo( 'description' ) ); ?>
-	</div>
-
-</div>
+<?php endif; ?>
