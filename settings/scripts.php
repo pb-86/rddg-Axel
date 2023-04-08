9a22020e7ca51cbd8ba24653c299eeb8309bdf39
Przemek Bąchorek <przemek@reddog.systems>
2023-04-08 17:39:15 +0200

    Dodano: Możliwość wyłączenia jQuery

--- settings/scripts.php
+++ settings/scripts.php
@@ -12,9 +12,14 @@ add_action( 'wp_enqueue_scripts', 'axel_scripts' );
  * Rejestruje i kolejkuje wszystkie pliki JS.
  */
 function axel_scripts() {
+	if ( ! is_admin() ) {
+		wp_deregister_script( 'jquery' );
+	}
+
 	wp_register_script( 'axel-dropdown-menu', esc_url( AXEL_THEME_SCRIPTS . 'dropdown-menu.min.js' ), array(), AXEL_THEME_VERSION, true );
 	wp_register_script( 'axel-scroll-to-top', esc_url( AXEL_THEME_SCRIPTS . 'scroll-to-top.min.js' ), array(), AXEL_THEME_VERSION, true );
 
 	wp_enqueue_script( 'axel-dropdown-menu' );
 	wp_enqueue_script( 'axel-scroll-to-top' );
+
 }
