c1be0ac41564c96faf15f61ec00b39ff6c46482c
Przemek Bąchorek <przemek@reddog.systems>
2023-08-08 11:07:20 +0200

    Zmieniono: Numer wersji

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @subpackage Axel
  */
 
-define( 'AXEL_THEME_VERSION', '1.76' );
+define( 'AXEL_THEME_VERSION', '1.77' );
 
 // Lista stałych do dalszego użycia.
 define( 'AXEL_THEME_URI', get_template_directory_uri() . '/' );
